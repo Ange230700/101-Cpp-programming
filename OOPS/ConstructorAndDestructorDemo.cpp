24551cff8e09bb4a9bf5ade2b48ffc986947105e
Ange230700 <angeericstephane.kouakou@ynov.com>
2023-02-24 20:15:10 +0100

    updates

---
+++ OOPS/ConstructorAndDestructorDemo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// class creation
+class CubeDemo
+{
+    // member variable and member function declaration
+public:
+    int side;
+
+    // constructor declaration
+    CubeDemo()
+    {
+        side = 8;
+    }
+    // destructor declaration
+    ~CubeDemo()
+    {
+        cout << "\nDestructor called...";
+    }
+};
+
+// declaration of main method
+
+int main()
+{
+    // object creation
+    CubeDemo cdemoobj;
+    cout << "";
+    cout << cdemoobj.side;
+}
---
+++ OOPS/UsingWsManipulator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    char empname[100];
+    cout << "Enter a employee name : ";
+
+    // ws is use to add white spaces
+    cin >> ws;
+    cin >> empname;
+    cout << "\n";
+    cout << "Employee name = " << empname;
+}
